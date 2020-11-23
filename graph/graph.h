a2c1d75918da27ecd0a83d559fd00c401d10026a
anityak2 <anityak2@illinois.edu>
2020-11-24 03:17:44 +0700

    graph changes w issues

--- graph/graph.h
+++ graph/graph.h
@@ -1,21 +1,23 @@
 #pragma once
 
-#include <string>
 #include <unordered_map>
 #include <vector>
+
+#include "../bitcoin/transaction.h"
+#include "../bitcoin/user.h"
 
 class Graph {
    public:
     Graph();
     ~Graph();
 
-    bool vertex_exists(std::string vertex);
-    bool edge_exists(std::string source, std::string target);
-    void insert_vertex(std::string vertex);
-    void insert_edge(std::string source, std::string target, int weight);
-    std::vector<std::string> get_adjacent(std::string vertex);
-    int get_weight(std::string source, std::string target);
+    bool vertex_exists(User vertex);
+    bool edge_exists(User source, User target);
+    void insert_vertex(User vertex);
+    void insert_edge(User source, User target, double weight);
+    std::vector<User> get_adjacent(User vertex);
+    double get_weight(User source, User target);
 
    private:
-    std::unordered_map<std::string, std::unordered_map<std::string, int>> adjList;
+    std::unordered_map<User, std::vector<Transaction>> adjList;
 };
