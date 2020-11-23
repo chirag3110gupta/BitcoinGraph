d499cbab213095ebafc4dd3661781bf62756f753
Adit Kapoor <aditk3@illinois.edu>
2020-11-23 23:37:15 +0530

    Reformatted code

--- graph.cpp
+++ graph.cpp
@@ -1,48 +1,43 @@
 #include "graph.h"
+
 #include <algorithm>
 
-graph::graph(){
-        //nothing to do here
+Graph::Graph() {
+    //nothing to do here
 }
 
-bool graph::edge_exists(std::string source, std::string target){
-        if(vertex_exists(source) && adjList.at(source).find(target) != adjList.at(source).end()){
-                return true;
-        }
-        return false;
+bool Graph::edge_exists(std::string source, std::string target) {
+    return vertex_exists(source) && adjList.at(source).find(target) != adjList.at(source).end();
 }
 
-bool graph::vertex_exists(std::string vertex){
-        if(adjList.find(vertex) != adjList.end()){
-                return true;
-        }
-        return false;
+bool Graph::vertex_exists(std::string vertex) {
+    return adjList.find(vertex) != adjList.end();
 }
 
-void graph::insert_vertex(std::string vertex){
-        adjList[vertex] = std::unordered_map<std::string, int>();
+void Graph::insert_vertex(std::string vertex) {
+    adjList[vertex] = std::unordered_map<std::string, int>();
 }
 
-void graph::insert_edge(std::string source, std::string target, int weight){
-        if(!vertex_exists(source)){
-                insert_vertex(source);
-        }
-        if(!vertex_exists(target)){
-                insert_vertex(target);
-        }
-        //what do you do if a source interacts with a target more than once and gives a different rating?
-        //Does this situation even exist in the dataset?
-        adjList[source][target] = weight;
+void Graph::insert_edge(std::string source, std::string target, int weight) {
+    if (!vertex_exists(source)) 
+        insert_vertex(source);
+    
+    if (!vertex_exists(target)) 
+        insert_vertex(target);
+    
+    //what do you do if a source interacts with a target more than once and gives a different rating?
+    //Does this situation even exist in the dataset?
+    adjList[source][target] = weight;
 }
 
-std::vector<std::string> graph::get_adjactent(std::string vertex){
-        std::vector<std::string> adjacent;
-        for(auto &obj : adjList[vertex]){
-                adjacent.push_back(obj.first);
-        }
-        return adjacent;
+std::vector<std::string> Graph::get_adjactent(std::string vertex) {
+    std::vector<std::string> adjacent;
+    for (auto &obj : adjList[vertex]) 
+        adjacent.push_back(obj.first);
+    
+    return adjacent;
 }
 
-int graph::get_weight(std::string source, std::string target){
-        return adjList[source][target];
+int Graph::get_weight(std::string source, std::string target) {
+    return adjList[source][target];
 }
--- graph.h
+++ graph.h
@@ -4,17 +4,17 @@
 #include <unordered_map>
 #include <vector>
 
-class graph{
-        private:
-                std::unordered_map<std::string, std::unordered_map<std::string, int>> adjList;
-        public:
-                graph();
-                ~graph();
-                bool vertex_exists(std::string vertex);
-                bool edge_exists(std::string source, std::string target);
-                void insert_vertex(std::string vertex);
-                void insert_edge(std::string source, std::string target, int weight);
-                std::vector<std::string> get_adjactent(std::string vertex);
-                int get_weight(std::string source, std::string target);
-                
+class Graph {public:
+    Graph();
+    ~Graph();
+    bool vertex_exists(std::string vertex);
+    bool edge_exists(std::string source, std::string target);
+    void insert_vertex(std::string vertex);
+    void insert_edge(std::string source, std::string target, int weight);
+    std::vector<std::string> get_adjactent(std::string vertex);
+    int get_weight(std::string source, std::string target);
+   private:
+    std::unordered_map<std::string, std::unordered_map<std::string, int>> adjList;
+
+   
 };
