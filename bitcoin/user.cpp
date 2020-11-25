93510725819dac3bda3b6e681e258b2187e1b864
chiragg4 <chiragg4@illinois.edu>
2020-11-25 16:19:44 +0530

    Done changes in user

--- bitcoin/user.cpp
+++ bitcoin/user.cpp
@@ -4,11 +4,11 @@ int User::numTransactions() {
     return _transToUser + _transFromUser;
 }
 void User::newTransaction(Transaction& trans) {
-    if (trans.getSource()->getUserID() == _userID)  
+    if (trans.source()->getUserID() == _userID)  
         _transFromUser++;
 
-    else if (trans.getTarget()->getUserID() == _userID) {
+    else if (trans.target()->getUserID() == _userID) {
         _transToUser++;
-        _avgRating = (_avgRating * (_transToUser - 1) + trans.getRating()) / _transToUser;
+        _avgRating = (_avgRating * (_transToUser - 1) + trans.rating()) / _transToUser;
     }
 }
