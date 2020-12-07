511677b738bf33c05ef0b2e3ee7fe875e654fa40
Adit Kapoor <aditk3@illinois.edu>
2020-12-07 07:50:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 
 int main() {
-    auto g = Graph("tests/test_data_headings.csv", true);
+    auto g = Graph("assets/data.csv", true);
     // auto g = Graph("assets/data.csv", true);
     g.printGraph();
 }
